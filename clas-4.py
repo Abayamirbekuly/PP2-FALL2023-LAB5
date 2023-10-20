741db56e046f8e5c3917c850924bc6d0019661b5
Abayamirbekuly <144439635+Abayamirbekuly@users.noreply.github.com>
2023-10-20 23:55:58 +0600

    Add files via upload

---
+++ clas-4.py
@@ -0,0 +1,37 @@
+import math
+
+class Point:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def show(self):
+        print(f"Coordinates: ({self.x}, {self.y})")
+
+    def move(self, new_x, new_y):
+        self.x = new_x
+        self.y = new_y
+
+    def dist(self, other_point):
+        dx = self.x - other_point.x
+        dy = self.y - other_point.y
+        distance = math.sqrt(dx**2 + dy**2)
+        return distance
+
+# Get user input for the coordinates of two points
+x1 = float(input("Enter the x-coordinate of the first point: "))
+y1 = float(input("Enter the y-coordinate of the first point: "))
+x2 = float(input("Enter the x-coordinate of the second point: "))
+y2 = float(input("Enter the y-coordinate of the second point: "))
+
+
+point1 = Point(x1, y1)
+point2 = Point(x2, y2)
+
+
+point1.show()
+point2.show()
+
+
+distance = point1.dist(point2)
+print(f"Distance between the two points: {distance:.2f}")
